f75f69cd033f5dd28893b59196740455b937c233
TheToasterMonster <frankqiang5040@gmail.com>
2022-07-01 15:54:40 -0400

    Initial Commit

---
+++ Checkers/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Checkers
+//
+//  Created by Frank Qiang on 7/1/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
